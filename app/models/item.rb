49b7f384b65029d0fa34847e56e775e7e8cca2bb
NicholasR77 <nl7robbins@icloud.com>
2020-08-26 20:22:36 -0700

    Fixed a minor issue with the Items model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
     belongs_to :user
-    has_many :items_meals
-    has_many :meals, through: :items_meals
+    has_many :meal_items
+    has_many :meals, through: :meal_items
 end
