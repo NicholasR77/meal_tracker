0227cef94234f642ac60a59f68d958679757ca32
NicholasR77 <nl7robbins@icloud.com>
2020-08-26 19:55:28 -0700

    Complete MVP

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -13,12 +13,12 @@ class Meal < ActiveRecord::Base
             :total_protein => 0
         }
         items.each do |item|
-            item.calories == nil ? totals[:total_calories] = 0 : totals[:total_calories] += item.calories
-            item.total_fat == nil ? totals[:total_fat] = 0 : totals[:total_fat] += item.total_fat
-            item.cholesterol == nil ? totals[:total_cholesterol] = 0 : totals[:total_cholesterol] += item.cholesterol
-            item.sodium == nil ? totals[:total_sodium] = 0 : totals[:sodium] += item.sodium
-            item.total_carbs == nil ? totals[:total_carbs] = 0 : totals[:total_carbs] += item.total_carbs
-            item.protein == nil ? totals[:total_protein] = 0 : totals[:total_protein] += item.protein
+            item.calories == nil ? totals[:total_calories] : totals[:total_calories] += item.calories
+            item.total_fat == nil ? totals[:total_fat] : totals[:total_fat] += item.total_fat
+            item.cholesterol == nil ? totals[:total_cholesterol] : totals[:total_cholesterol] += item.cholesterol
+            item.sodium == nil ? totals[:total_sodium] : totals[:total_sodium] += item.sodium
+            item.total_carbs == nil ? totals[:total_carbs] : totals[:total_carbs] += item.total_carbs
+            item.protein == nil ? totals[:total_protein] : totals[:total_protein] += item.protein
         end
         totals
     end
